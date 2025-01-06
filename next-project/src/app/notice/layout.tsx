8aa2825abd4ec24fe9934888066c318c6dce9c2e
jongjunpark <poiufgin7373@naver.com>
2025-01-06 16:16:53 +0900

    chore: next initialize

---
+++ next-project/src/app/notice/layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+const NoticeLayout = ({ children }: PropsWithChildren) => {
+  return <section>{children}</section>;
+};
+
+export default NoticeLayout;
